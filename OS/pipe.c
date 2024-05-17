cc8d3068fcb20554dddb84318bd52c46b2ca67c9
grinya <fxckingoups@vk.com>
2024-05-17 10:19:26 +0700

    Изменили файл, передающий информацию через pipe

--- OS/pipe.c
+++ OS/pipe.c
@@ -3,20 +3,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main() {
-	int fd[2];
+int main(), {
+	int fd[2], result;
 	size_t size;
 	char string[] = "Hello from pipe!\n";
 	char resstring [17];
-	if (pipe(fd)<0) {
-		printf("Не удалось создать pipe\n");
-		exit(-1);
+	 if (pipe(fd)<0) {
+                printf("Не удалось создать pipe\n");
+                exit(-1);
+        }
+	result = fork();
+	if (result <0) {
+		printf("Ошибка при запуске fork()\n");
+		exit (-1);
 	}
-	size = write(fd[1], string, 17);
-	if (size != 17) {
-		printf("Не удалось записать 17 байт из строки\n");
-		exit(-1);
-
+	else if (result >0) {
+		size = write(fd[1], string, 17);
+		if (size != 17) {
+			printf("Не удалось записать 17 байт из строки\n");
+			exit(-1);
+	}
+		printf("Родитель записал в строку в pipe\n");
+	}
+	else {
+		if (close(fd[1])<0) {
+                printf("Не удалось закрыть pipe для чтения\n");
+                exit(-1);
 	}
 	size = read(fd[0], resstring, 17);
 	if (size !=17) {
@@ -27,10 +39,6 @@ int main() {
 	if (close(fd[0])<0) {
 		printf("Не удалось закрыть pipe для чтения\n");
 		exit(-1);
-	}
-	if (close(fd[1])<0) {
-                printf("Не удалось закрыть pipe для чтения\n");
-                exit(-1);
 	}
 	return 0;
 }
